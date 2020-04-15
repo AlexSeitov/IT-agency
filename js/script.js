b9c25343b61ff42133ea8ee9f73e14c04379d923
Alex Seitov <seitov_ai@mail.ru>
2020-04-15 19:48:49 +0300

    Сверстал блок Works

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 // mobile menu
 
-let menuToggle = document.querySelector('.nav-toggle');
-let mobileNavContainer = document.querySelector('.nav');
+$(".nav-toggle").on("click", function(event) {
+    event.preventDefault();
 
-menuToggle.onclick = function(){
-    menuToggle.classList.toggle('nav-toggle--active');
-    mobileNavContainer.classList.toggle('nav--active');
-}
+    $(this).toggleClass("nav-toggle--active");
+    $(".nav").toggleClass("nav--active");
+});
+
+// isotope
+
+$('.works-gallery').isotope({
+    itemSelector: '.works-gallery_item',
+    layoutMode: 'fitRows',
+});
+
+$('.works-menu__item').click(function() {
+    $('.works-menu__item').removeClass('works-menu__item--active');
+    $(this).addClass('works-menu__item--active');
+
+    let selector = $(this).attr('data-filter');
+    $('.works-gallery').isotope ({
+        filter: selector
+    });
+    return false;
+});
