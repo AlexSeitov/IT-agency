8c284c7fd8d31423dba432709a350c39d9af16a0
Alex Seitov <seitov_ai@mail.ru>
2020-04-17 16:08:16 +0300

    Исправлены ошибки

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-// Fixed Header
-
 $(function() {
+
+    // Fixed Header
+
     var header = $("#header"),
-        featuresH = $("#features").innerHeight(),
+        mainblockH = 200,
         scrollOffset = $(window).scrollTop();
 
     checkScroll(scrollOffset);
@@ -15,55 +16,55 @@ $(function() {
 
 
     function checkScroll(scrollOffset) {
-        if( scrollOffset >= featuresH ) {
+        if( scrollOffset >= mainblockH ) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
     }
-});
 
-// Smooth scroll
+    // Smooth scroll
 
-$("[data-scroll]").on("click", function(event) {
-    event.preventDefault();
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
 
-    let $this = $(this),
-        blockId = $this.data('scroll'),
-        blockOffset = $(blockId).offset().top;
+        let $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
 
-    $(".nav__link").removeClass("nav__link--active");
-    $this.addClass("nav__link--active");
+        $(".nav__link").removeClass("nav__link--active");
+        $this.addClass("nav__link--active");
 
-    $("html, body").animate({
-        scrollTop:  blockOffset
-    }, 500);
-});
-
-
-// Mobile menu
-
-$(".nav-toggle").on("click", function(event) {
-    event.preventDefault();
-
-    $(this).toggleClass("nav-toggle--active");
-    $(".nav").toggleClass("nav--active");
-});
-
-// Isotope
-
-$('.works-gallery').isotope({
-    itemSelector: '.works-gallery_item',
-    layoutMode: 'fitRows',
-});
-
-$('.works-menu__item').click(function() {
-    $('.works-menu__item').removeClass('works-menu__item--active');
-    $(this).addClass('works-menu__item--active');
-
-    let selector = $(this).attr('data-filter');
-    $('.works-gallery').isotope ({
-        filter: selector
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 500);
     });
-    return false;
+
+
+    // Mobile menu
+
+    $(".nav-toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("nav-toggle--active");
+        $(".nav").toggleClass("nav--active");
+    });
+
+    // Isotope
+
+    $('.works-gallery').isotope({
+        itemSelector: '.works-gallery_item',
+        layoutMode: 'fitRows',
+    });
+
+    $('.works-menu__item').click(function() {
+        $('.works-menu__item').removeClass('works-menu__item--active');
+        $(this).addClass('works-menu__item--active');
+
+        let selector = $(this).attr('data-filter');
+        $('.works-gallery').isotope ({
+            filter: selector
+        });
+        return false;
+    });
 });
